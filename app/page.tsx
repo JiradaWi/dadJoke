edd29f1b2c4732e090af2575b07d8337278a9cf8
jiradawi <cutiemoon08@yahoo.com>
2025-04-05 22:10:23 +0700

    fix search and auto focus input

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
 
   useEffect(() => {
     setIsLoading('true');
-    newJoke();
+    newJoke('');
   }, []);
 
   const searchTextHandle = (e: any) => {
@@ -17,10 +17,22 @@ export default function Home() {
     setSearchText(value);
   }
 
-  const newJoke = () => {
-    console.log('newJoke')
+  const searchTextPress = (e: any) =>{
+    if(e.keyCode == 13){
+      const value = e.target.value;
+      console.log('value  '+value );
+      setSearchText(value);
+      newJoke(value);
+    }
+  }
+
+  const handleNewJokeClick = () => {
+    newJoke(searchText);
+  }
+
+  const newJoke = (searchText: string) => {
     setIsLoading('true');
-   
+    
     if(searchText == ''){
       fetch('https://icanhazdadjoke.com/', {
         headers: {
@@ -85,15 +97,15 @@ export default function Home() {
                 id="search" 
                 className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
                 placeholder="Search" 
+                autoFocus
+                onKeyUp={searchTextPress}
                 onBlur={searchTextHandle}
                  />
-              <button type="submit" onClick={newJoke} 
+              <button type="submit" onClick={handleNewJokeClick} 
               className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 Search Joke
               </button>
-          </div>
-
-           
+          </div>           
         </div>
         );
     }
